e9a178642c2fff9dc7c40516f71d0f5511fa35a0
stackinspector <stackinspector@126.com>
2023-05-24 02:29:17 +0800

    Absorb Squeeze Once & restore rand

--- cshake/lib.rs
+++ cshake/lib.rs
@@ -6,11 +6,6 @@ use keccak_core::{KeccakState, KeccakF};
 #[cfg(feature = "zeroize-on-drop")]
 use zeroize::Zeroize;
 
-pub struct CShake<C: CShakeCustom> {
-    ctx: KeccakState<KeccakF>,
-    custom: C,
-}
-
 fn init(name: &[u8], custom_string: &[u8]) -> KeccakState<KeccakF> {
     use keccak_core::{bits_to_rate, DELIM_CSHAKE, DELIM_SHAKE};
     let rate = bits_to_rate(256);
@@ -30,57 +25,53 @@ fn init(name: &[u8], custom_string: &[u8]) -> KeccakState<KeccakF> {
     }
 }
 
-impl<C: CShakeCustom> CShake<C> {
-    #[inline]
-    pub fn custom(&self) -> &C {
-        &self.custom
-    }
-
-    #[inline]
-    pub fn absorb(&mut self, input: &[u8]) {
-        self.ctx.absorb(input);
-    }
-
-    #[inline]
-    pub fn chain_absorb(mut self, input: &[u8]) -> CShake<C> {
-        self.ctx.absorb(input);
+pub trait Absorb: Sized {
+    fn absorb(&mut self, input: &[u8]);
+    
+    #[inline(always)]
+    fn chain_absorb(mut self, input: &[u8]) -> Self {
+        self.absorb(input);
         self
     }
+}
+
+pub trait Squeeze {
+    fn squeeze(&mut self, output: &mut [u8]);
 
     #[inline]
-    pub fn squeeze(&mut self, output: &mut [u8]) {
-        self.ctx.squeeze(output);
-    }
-
-    #[inline]
-    pub fn squeeze_to_array<const N: usize>(&mut self) -> [u8; N] {
+    fn squeeze_to_array<const N: usize>(&mut self) -> [u8; N] {
         let mut buf = [0; N];
-        self.ctx.squeeze(&mut buf);
+        self.squeeze(&mut buf);
         buf
     }
 
     #[cfg(feature = "alloc")]
     #[inline]
-    pub fn squeeze_to_vec(&mut self, len: usize) -> Vec<u8> {
+    fn squeeze_to_vec(&mut self, len: usize) -> Vec<u8> {
         // TODO use MaybeUninit
         let mut buf = vec![0; len];
-        self.ctx.squeeze(&mut buf);
+        self.squeeze(&mut buf);
         buf
     }
 
-    // TODO(below 3 methods): necessary to zeroize?
-    // TODO(below 5 methods): inline?
+    // TODO: necessary to zeroize?
 
-    #[inline]
-    pub fn skip<const N: usize>(&mut self) {
+    fn skip_const<const N: usize>(&mut self) {
         #[allow(unused_variables, unused_mut)]
         let mut buf = self.squeeze_to_array::<N>();
         #[cfg(feature = "zeroize-on-drop")]
         buf.zeroize();
     }
 
-    #[inline]
-    pub fn squeeze_xor_array<const N: usize>(&mut self, dest: &mut [u8; N]) {
+    #[cfg(feature = "alloc")]
+    fn skip(&mut self, len: usize) {
+        #[allow(unused_variables, unused_mut)]
+        let mut buf = self.squeeze_to_vec(len);
+        #[cfg(feature = "zeroize-on-drop")]
+        buf.zeroize();
+    }
+
+    fn squeeze_xor_array<const N: usize>(&mut self, dest: &mut [u8; N]) {
         #[allow(unused_mut)]
         let mut mask = self.squeeze_to_array();
         xor::xor_array(dest, &mask);
@@ -89,8 +80,7 @@ impl<C: CShakeCustom> CShake<C> {
     }
 
     #[cfg(feature = "alloc")]
-    #[inline]
-    pub fn squeeze_xor_slice(&mut self, dest: &mut [u8]) {
+    fn squeeze_xor_slice(&mut self, dest: &mut [u8]) {
         #[allow(unused_mut)]
         let mut mask = self.squeeze_to_vec(dest.len());
         // hardcode inline without reslicing because no need to check
@@ -98,20 +88,48 @@ impl<C: CShakeCustom> CShake<C> {
         #[cfg(feature = "zeroize-on-drop")]
         mask.zeroize();
     }
+}
 
+pub trait Once: Absorb + Squeeze {
     #[inline]
-    pub fn once(mut self, input: &[u8], output: &mut [u8]) {
-        self.ctx.absorb(input);
-        self.ctx.squeeze(output);
+    fn once(mut self, input: &[u8], output: &mut [u8]) {
+        self.absorb(input);
+        self.squeeze(output);
     }
 
     #[inline]
-    pub fn once_to_array<const N: usize>(mut self, input: &[u8]) -> [u8; N] {
-        self.ctx.absorb(input);
+    fn once_to_array<const N: usize>(mut self, input: &[u8]) -> [u8; N] {
+        self.absorb(input);
         self.squeeze_to_array()
     }
+}
 
-    #[inline]
+impl<T: Absorb + Squeeze> Once for T {}
+pub struct CShake<C: CShakeCustom> {
+    ctx: KeccakState<KeccakF>,
+    custom: C,
+}
+
+impl<C: CShakeCustom> Absorb for CShake<C> {
+    #[inline(always)]
+    fn absorb(&mut self, input: &[u8]) {
+        self.ctx.absorb(input);
+    }
+}
+
+impl<C: CShakeCustom> Squeeze for CShake<C> {
+    #[inline(always)]
+    fn squeeze(&mut self, output: &mut [u8]) {
+        self.ctx.squeeze(output);
+    }
+}
+
+impl<C: CShakeCustom> CShake<C> {
+    #[inline(always)]
+    pub fn custom(&self) -> &C {
+        &self.custom
+    }
+
     pub fn squeeze_to_ctx<const N: usize, C2: CShakeCustom>(&mut self, custom: C2) -> CShake<C2> {
         #[allow(unused_mut)]
         let mut buf = self.squeeze_to_array::<N>();
@@ -150,4 +168,35 @@ macro_rules! cshake_customs {
             const CUSTOM_STRING: &'static str = concat!($prefix, stringify!($name));
         }
     )*};
+}
+
+#[cfg(feature = "rand")]
+pub mod rand {
+    use std::{thread_local, rc::Rc, cell::UnsafeCell};
+    use crate::{CShake, Absorb, Squeeze, CShakeCustom, NoCustom};
+
+    fn init() -> CShake<NoCustom> {
+        let mut buf = [0; 32];
+        getrandom::getrandom(&mut buf).unwrap();
+        let ctx = NoCustom.create().chain_absorb(&buf);
+        ctx
+    }
+
+    thread_local! {
+        static THREAD_RNG: Rc<UnsafeCell<CShake<NoCustom>>> = Rc::new(UnsafeCell::new(init()));
+    }
+
+    pub struct ThreadRng(Rc<UnsafeCell<CShake<NoCustom>>>);
+
+    pub fn thread_rng() -> ThreadRng {
+        ThreadRng(THREAD_RNG.with(Clone::clone))
+    }
+
+    impl Squeeze for ThreadRng {
+        #[inline(always)]
+        fn squeeze(&mut self, output: &mut [u8]) {
+            let ctx = unsafe { &mut *self.0.get() };
+            ctx.squeeze(output);
+        }
+    }
 }
