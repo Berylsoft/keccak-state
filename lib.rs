b783eeb8617e30c5e37d7c9062cdd8f3dcdb3b97
stackinspector <stackinspector@126.com>
2023-05-25 09:28:57 +0800

    use rust crypto keccak fn & WORDS BYTES

--- lib.rs
+++ lib.rs
@@ -1,130 +1,36 @@
 #![no_std]
-#![allow(non_upper_case_globals)]
+#![allow(non_upper_case_globals, non_snake_case)]
 
-const RHO: [u32; 24] = [
-    1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 2, 14, 27, 41, 56, 8, 25, 43, 62, 18, 39, 61, 20, 44,
-];
-
-const PI: [usize; 24] = [
-    10, 7, 11, 17, 18, 3, 5, 16, 8, 21, 24, 4, 15, 23, 19, 13, 12, 2, 20, 14, 22, 9, 6, 1,
-];
-
-const WORDS: usize = BITS / 64;
-const BYTES: usize = BITS / 8;
 const BITS: usize = 1600;
 
-const KECCAK_F_RC: [u64; 24] = [
-    1,
-    0x8082,
-    0x800000000000808a,
-    0x8000000080008000,
-    0x808b,
-    0x80000001,
-    0x8000000080008081,
-    0x8000000000008009,
-    0x8a,
-    0x88,
-    0x80008009,
-    0x8000000a,
-    0x8000808b,
-    0x800000000000008b,
-    0x8000000000008089,
-    0x8000000000008003,
-    0x8000000000008002,
-    0x8000000000000080,
-    0x800a,
-    0x800000008000000a,
-    0x8000000080008081,
-    0x8000000000008080,
-    0x80000001,
-    0x8000000080008008,
-];
+pub const fn WORDS(bits: usize) -> usize {
+    bits / 64
+}
 
-const KECCAK_P_RC: [u64; 12] = [
-    0x8000808b,
-    0x800000000000008b,
-    0x8000000000008089,
-    0x8000000000008003,
-    0x8000000000008002,
-    0x8000000000000080,
-    0x800a,
-    0x800000008000000a,
-    0x8000000080008081,
-    0x8000000000008080,
-    0x80000001,
-    0x8000000080008008,
-];
+pub const fn BYTES(bits: usize) -> usize {
+    bits / 8
+}
+
+pub const KeccakF: bool = true;
+pub const KeccakP: bool = false;
+
+pub const fn bits_to_rate(bits: usize) -> usize {
+    200 - bits / 4
+}
+
+pub const R128: usize = bits_to_rate(128);
+pub const R224: usize = bits_to_rate(224);
+pub const R256: usize = bits_to_rate(256);
+pub const R384: usize = bits_to_rate(384);
+pub const R512: usize = bits_to_rate(512);
+
+pub const DKeccak : u8 = 0x01;
+pub const DSHA3   : u8 = 0x06;
+pub const DSHAKE  : u8 = 0x1f;
+pub const DCSHAKE : u8 = 0x04;
 
 #[cfg(feature = "zeroize-on-drop")]
 use zeroize::Zeroize;
-
-#[allow(unused_assignments, non_snake_case)]
-#[inline(always)]
-pub fn keccak<const ROUNDS: usize>(a: &mut [u64; WORDS], RC: &[u64; ROUNDS]) {
-    for i in 0..ROUNDS {
-        use crunchy::unroll;
-
-        let mut array: [u64; 5] = [0; 5];
-
-        // Theta
-        unroll! {
-            for x in 0..5 {
-                unroll! {
-                    for y_count in 0..5 {
-                        let y = y_count * 5;
-                        array[x] ^= a[x + y];
-                    }
-                }
-            }
-        }
-
-        unroll! {
-            for x in 0..5 {
-                unroll! {
-                    for y_count in 0..5 {
-                        let y = y_count * 5;
-                        a[y + x] ^= array[(x + 4) % 5] ^ array[(x + 1) % 5].rotate_left(1);
-                    }
-                }
-            }
-        }
-
-        // Rho and pi
-        let mut last = a[1];
-        unroll! {
-            for x in 0..24 {
-                array[0] = a[PI[x]];
-                a[PI[x]] = last.rotate_left(RHO[x]);
-                last = array[0];
-            }
-        }
-
-        // Chi
-        unroll! {
-            for y_step in 0..5 {
-                let y = y_step * 5;
-
-                unroll! {
-                    for x in 0..5 {
-                        array[x] = a[y + x];
-                    }
-                }
-
-                unroll! {
-                    for x in 0..5 {
-                        a[y + x] = array[x] ^ ((!array[(x + 1) % 5]) & (array[(x + 2) % 5]));
-                    }
-                }
-            }
-        };
-
-        // Iota
-        a[0] ^= RC[i];
-
-        #[cfg(feature = "zeroize-on-drop")]
-        array.zeroize()    
-    }
-}
 
 #[derive(Clone, Copy)]
 enum Mode {
@@ -134,7 +40,7 @@ enum Mode {
 use Mode::*;
 
 pub struct KeccakState<const P: bool, const R: usize> {
-    buf: [u8; BYTES],
+    buf: [u8; BYTES(BITS)],
     offset: usize,
     delim: u8,
     mode: Mode,
@@ -164,7 +70,7 @@ impl<const P: bool, const R: usize> KeccakState<P, R> {
         // TODO complie time
         assert!(R != 0, "rate cannot be equal 0");
         KeccakState {
-            buf: [0; BYTES],
+            buf: [0; BYTES(BITS)],
             offset: 0,
             delim,
             mode: Absorbing,
@@ -209,10 +115,10 @@ impl<const P: bool, const R: usize> KeccakState<P, R> {
     }
 
     fn keccak(&mut self) {
-        let words: &mut [u64; WORDS] = unsafe { core::mem::transmute(&mut self.buf) };
+        let words: &mut [u64; WORDS(BITS)] = unsafe { core::mem::transmute(&mut self.buf) };
         #[cfg(target_endian = "big")]
         #[inline]
-        fn swap_endianess(words: &mut [u64; WORDS]) {
+        fn swap_endianess(words: &mut [u64; WORDS(BITS)]) {
             for item in words {
                 *item = item.swap_bytes();
             }
@@ -220,9 +126,9 @@ impl<const P: bool, const R: usize> KeccakState<P, R> {
         #[cfg(target_endian = "big")]
         swap_endianess(words);
         if P == KeccakF {
-            keccak::<{ KECCAK_F_RC.len() }>(words, &KECCAK_F_RC);
+            keccak::f1600(words);
         } else {
-            keccak::<{ KECCAK_P_RC.len() }>(words, &KECCAK_P_RC);
+            keccak::p1600(words, 12);
         }
         #[cfg(target_endian = "big")]
         swap_endianess(words);
@@ -275,7 +181,7 @@ impl<const P: bool, const R: usize> KeccakState<P, R> {
         #[cfg(feature = "zeroize-on-drop")]
         self.buf.zeroize();
         #[cfg(not(feature = "zeroize-on-drop"))]
-        let _ = core::mem::replace(&mut self.buffer, [0; BYTES]);
+        let _ = core::mem::replace(&mut self.buffer, [0; BYTES(BITS)]);
         self.offset = 0;
         self.mode = Absorbing;
     }
@@ -310,20 +216,3 @@ impl<const P: bool, const R: usize> KeccakState<P, R> {
     }
 }
 
-pub const fn bits_to_rate(bits: usize) -> usize {
-    200 - bits / 4
-}
-
-pub const DKeccak : u8 = 0x01;
-pub const DSHA3   : u8 = 0x06;
-pub const DSHAKE  : u8 = 0x1f;
-pub const DCSHAKE : u8 = 0x04;
-
-pub const KeccakF: bool = true;
-pub const KeccakP: bool = false;
-
-pub const R128: usize = bits_to_rate(128);
-pub const R224: usize = bits_to_rate(224);
-pub const R256: usize = bits_to_rate(256);
-pub const R384: usize = bits_to_rate(384);
-pub const R512: usize = bits_to_rate(512);
