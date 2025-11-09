4f97256684b6a8896d5b96afa5e559084a4349a6
stackinspector <stackinspector@126.com>
2025-11-09 18:36:43 +0800

    web: helloworld test

--- cshake-web/lib.rs
+++ cshake-web/lib.rs
@@ -8,3 +8,16 @@ pub fn shake256_once(data: &[u8], len: u32) -> Vec<u8> {
         .chain_absorb(data)
         .squeeze_to_vec(len as usize)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(
+            shake256_once("Hello, World!".as_bytes(), 32),
+            hex_literal::hex!("b3be97bfd978833a65588ceae8a34cf59e95585af62063e6b89d0789f372424e"),
+        )
+    }
+}
