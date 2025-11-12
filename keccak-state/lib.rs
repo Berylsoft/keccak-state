b1baecc89be6a7aa1fbd60c94502b164415597af
stackinspector <stackinspector@126.com>
2025-11-12 18:17:40 +0800

    upgrade getrandom = "0.3.4"

--- keccak-state/lib.rs
+++ keccak-state/lib.rs
@@ -282,7 +282,7 @@ pub trait AbsorbSeed: Absorb {
     fn absorb_seed<const N: usize>(&mut self) {
         use core::mem::MaybeUninit;
         let mut buf: [MaybeUninit<u8>; N] = unsafe { MaybeUninit::uninit().assume_init() };
-        let ready_buf = getrandom::getrandom_uninit(&mut buf).unwrap();
+        let ready_buf = getrandom::fill_uninit(&mut buf).unwrap();
         self.absorb(ready_buf);
         #[cfg(feature = "zeroize-on-drop")]
         buf.zeroize();
